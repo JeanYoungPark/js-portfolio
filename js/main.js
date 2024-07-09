d127a075de49ca816fa1d2edc3f46c8d9620eeeb
JeanYoungPark <jean.young.park.96@gmail.com>
2024-07-09 15:46:53 +0900

    feat: menu 수정

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const menuPopup = document.getElementById("menu-popup");
 menuIcon.addEventListener("click", () => {
     menuPopup.classList.add("active");
 });
+
+const closeIcon = document.getElementById("close");
+
+closeIcon.addEventListener("click", () => {
+    menuPopup.classList.remove("active");
+});
