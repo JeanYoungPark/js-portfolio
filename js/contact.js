3e762e41febb0c9c45b0837312387746e1eaf70b
JeanYoungPark <jean.young.park.96@gmail.com>
2024-07-26 11:26:04 +0900

    dev: contact 페이지 완료

--- js/contact.js
+++ js/contact.js
@@ -4,34 +4,54 @@ VanillaTilt.init(document.querySelector(".card"), {
     glare: true,
 });
 
-emailjs.init({
-    publicKey: "YOUR_PUBLIC_KEY",
-    // Do not allow headless browsers
-    blockHeadless: true,
-    blockList: {
-        // Block the suspended emails
-        list: ["foo@emailjs.com", "bar@emailjs.com"],
-        // The variable contains the email address
-        watchVariable: "userEmail",
-    },
-    limitRate: {
-        // Set the limit rate for the application
-        id: "app",
-        // Allow 1 request per 10s
-        throttle: 10000,
-    },
-});
+// emailjs.init({
+//     publicKey: "YOUR_PUBLIC_KEY",
+//     // Do not allow headless browsers
+//     blockHeadless: true,
+//     blockList: {
+//         // Block the suspended emails
+//         list: ["foo@emailjs.com", "bar@emailjs.com"],
+//         // The variable contains the email address
+//         watchVariable: "userEmail",
+//     },
+//     limitRate: {
+//         // Set the limit rate for the application
+//         id: "app",
+//         // Allow 1 request per 10s
+//         throttle: 10000,
+//     },
+// });
 
-let templateParams = {
-    name: "James",
-    notes: "Check this out!",
+// let templateParams = {
+//     name: "James",
+//     notes: "Check this out!",
+// };
+
+// emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams).then(
+//     (response) => {
+//         console.log("SUCCESS!", response.status, response.text);
+//     },
+//     (error) => {
+//         console.log("FAILED...", error);
+//     }
+// );
+
+const liquid = () => {
+    let contact = document.querySelector(".liquid-box");
+    let e = document.createElement("div");
+    e.setAttribute("class", "drops");
+    contact.appendChild(e);
+
+    let size = Math.random() * 100;
+    e.style.width = 15 + size + "px";
+    let contactWidth = contact.clientWidth;
+    e.style.left = Math.random() * contactWidth + "px";
+
+    setTimeout(() => {
+        contact.removeChild(e);
+    }, 5000);
 };
 
-emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams).then(
-    (response) => {
-        console.log("SUCCESS!", response.status, response.text);
-    },
-    (error) => {
-        console.log("FAILED...", error);
-    }
-);
+setInterval(() => {
+    liquid();
+}, 500);
